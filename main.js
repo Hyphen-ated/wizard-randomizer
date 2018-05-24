970ea63afea201647907da8ace1a3f1e76b974bf
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2018-05-24 00:53:15 -0700

    initial version, things are working

---
+++ main.js
@@ -0,0 +1,298 @@
+outfits = [
+"Hope",
+"Patience",
+"Vigor",
+"Pace",
+"Grit",
+"Shift",
+"Tempo",
+"Awe",
+"Rule",
+"Venture",
+"Pride",
+"Savile Special"
+];
+
+offenseRelics = [
+"Amulet of Sundering",
+"Idealist's Mirror",
+"Fortune Cookie",
+"Berserker's Helm",
+"Merchant's Cart",
+"Sinister Ledger",
+"Ifrit's Matchstick",
+"Rudra's Pinwheel",
+"Gaia's Shovel",
+"Battery of Taranis",
+"Shiva's Water Bottle",
+"Stanza of Frost",
+"Ebon Wolf's Cloak",
+"Captain's Ring",
+"Royal Flush",
+"Dagger of Midas",
+"Berserker's Axe",
+"Cartographer's Quill",
+"Hunter's Stiletto",
+"Phoenix Talon",
+"Nerite Shell",
+"Noxious Dappercap",
+"Tesla Coil",
+"Permafrost Cube",
+"Agni's Sparkler",
+"Devi's Bug Spray",
+"Indra's Copper Pipe",
+"Analytical Module",
+"Assassin's Blade",
+"Dark Katana",
+"Tapping Gloves",
+"Covert Ops Mask",
+"Auditor's Talisman",
+"Singing Bowl",
+"Dice of the Nemesis",
+"Autograph Pad",
+"Wanderer's Mechanism",
+"Ancient Fountain Pen",
+"Secret Wild Card",
+"Cobalt Firestone",
+"Sleepy Thunderstone",
+"Lotus Froststone",
+"Sidewinder's Badge",
+"Whimsical Explodium",
+"Special Snowflake",
+"Neve's Ruby",
+"Neve's Citrine",
+"Friendship Bracelet"
+];
+
+
+defenseRelics = [
+"Giant's Heart",
+"Tea of Mercy",
+"Super Carrot Cake",
+"Raspberry Cookie Box",
+"Vampire's Eyeglasses",
+"Regenerative Inkwell",
+"Jewelry Box",
+"Pathfinder's Knapsack",
+"Pazu's Favorite Hat",
+"Scissors of Vitality",
+"Kali's Flower Diadem",
+"Tracking Suit",
+"Resolute Svalinn",
+"Auger of Poetry",
+"Three Gorges Bulwark",
+"Super Sunscreen",
+"Gummy Vitamins",
+"Fuzzy Handwarmers",
+"Euphie's Shawl",
+"Armor of Resolve",
+"Heavy Travel Jacket",
+"Memory Chainmail",
+"Calcifying Bonemail",
+"Thesis on Defense",
+"Antiquated Tabi",
+"Puffy Parka",
+"Calvin's Sandy Shoes",
+"Mercury's Sandals",
+"Equestricap",
+"Spell Thief's Socks",
+"Greased Boots",
+"Leemo's Leaf",
+"Takeout Box",
+"Cushioned Flip-Flops",
+"Ring of Recycling",
+"Bracers of the Beast",
+"Demi's Teapot",
+"Hummingbird Feather",
+"Elven Ears",
+"Mirror Shield",
+"Limited Edition Robe",
+"Deafening Cymbals",
+"Reactive Vinemail",
+"Lei's Drum",
+"Charming Teddy Bear",
+"Stygian Turtle Shell",
+"Neve's Quartz",
+"Neve's Emerald"
+];
+
+miscRelics = [
+"Museum Ticket",
+"Roxel's Pendulum",
+"Tozy's Pocket Watch",
+"Elemental Pointes",
+"Spice Rack",
+"Nocturnal Sundial",
+"Phyyrnx's Hourglass",
+"Destructive Abacus",
+"Boots of Frenzy",
+"Yuna's Storybook",
+"Grimoire of Ruin",
+"Pop-up Primer",
+"Igniting Sprite Vesa",
+"Blasting Sprite Aura",
+"Plaguing Sprite Dria",
+"Sparing Sprite Etra",
+"Freezing Sprite Naya",
+"Mystic Monopole",
+"Surefire Rocket",
+"Infinity Marble",
+"Albert's Formula",
+"Hyperbolic Train",
+"Absorption Coil",
+"Chaos Scanner",
+"Combo Gloves",
+"Evening Gloves",
+"Flak Gauntlet",
+"Reinforced Bracers",
+"Dated Sunglasses",
+"Bladed Buckler",
+"Ring of Reserves",
+"Sano's Headband",
+"Pathfinder's Map",
+"Glove of Midas",
+"Journal of Midas",
+"Tears of Midas",
+"Relic Rewards Card",
+"Arcana Rewards Card",
+"Raffle Ticket",
+"Supply Crate",
+"Wallet of Vigor",
+"C-99 Piggy Bank",
+"Conqueror's Helmet",
+"Conqueror's Belt",
+"Conqueror's Cloak",
+"Neve's Sapphire",
+"Unicorn Tail",
+"Insignia of Legend"
+];
+
+basicArcana = {
+    "Fire": ["Flame Strike", "Bouncing Blaze"],
+    "Air": ["Wind Slash", "Air Spinner"],
+    "Earth": ["Earth Knuckles", "Stone Shot"],
+    "Lightning": ["Spark Contact", "Bolt Rail"],
+    "Water": ["Ice Dagger", "Aqua Arc"],
+    "Chaos": ["Chaos Crusher"]
+}
+
+dashArcana = {
+    "Fire": ["Searing Rush", "Flare Rush"],
+    "Air": ["Air Burst", "Gust Burst"],
+    "Earth": ["Spike Track", "Toxic Trap"],
+    "Lightning":["Curcuit Line", "Shock Line"],
+    "Water": ["Wave Front", "Frost Feint"],
+    "Chaos": ["Chaotic Rift"]
+}
+
+standardArcana = {
+    "Fire": ["Fuelled Berserk", "Flame Breath", "Searing Crown", "Blazing Lariat", "Blazing Vault", "Explosive Charge", "Blazing Blitz", "Blazing Onslaught", "Homing Flares", "Tracer Barrage", "Dragon Arc", "Exploding Fireball", "Flame Cleaver", "Flame Fusion", "Raging Inferno", "Ignition Drive", "Fire Wall", "Crashing Meteor", "Rapid Fire Agent", "Ward of Flames"],
+    "Air": ["Evading Zephyr", "Spiraling Typhoon", "Dragon Blast", "Whirling Tornado", "Scales of Babylon", "Mentis Imperium", "Airborne Slam", "Heroic Leap", "Tearing Whirlwind", "Shearing Chain", "Rushing Typhoon", "Wind Flurry", "Storm Draft", "Cyclone Boomerang", "Gust Volley", "Furious Tempest", "Breaking Twister", "Mach Stunner", "Whirling Wind Agent", "Ward of Winds"],
+    "Earth": ["Magnetic Follow-Up", "Shattering Strike", "Fadeaway Rhythm", "Stalwart Defenders", "Earthen Aegis", "Terra Ring", "Grasping Earth", "Seismic Entry", "Tectonic Drill", "Aegis Charge", "Knockout Boulder", "Toxic Bolas", "Obsidian Cascade", "Rock N' Roll", "Dragon Dive", "Rippling Rupture", "Earthquake Axe", "Embracing Vines", "Earth Stomp Agent", "Ward of Stones"],
+    "Lightning":["Lightning Aspect", "Arc Sphere", "Magsphere", "Shock Nova", "Thunder Drop", "Voltaic Needle", "Shock Assault", "Volt Pinball", "Star Bolt", "Deferred Dynamo", "Bouncing Surge", "Ball Lightning", "Bolt Claymore", "Spark Array", "Thundering Chain", "Ion Spike", "Surge Anchor", "Shock Wire", "Line Shock Agent", "Ward of Lightning"],
+    "Water": ["Creeping Tendrils", "Tidal Blast", "Aqua Vortex", "Cardice Prime", "Snowflake Chakrams", "Binary Splashdown", "Glacial Cross", "Water Prison", "Bouncing Bubble", "Ice Spear", "Aqua Breaker", "Bubble Barrage", "Frost Fan", "Rebounding Icicles", "Aqua Beam", "Perfect Storm", "Frozen Bait", "Ice Seekers", "Water Pulse Agent", "Ward of Frost"],
+    "Chaos": ["Mortal Coil", "Arcane Intervention", "Null Parade", "Distortion Beam", "Phantom Brigade", "Absolute Finale"]
+}
+
+signatureArcana = {
+    "Fire": ["Flame Breath", "Blazing Lariat", "Tracer Barrage", "Exploding Fireball"],
+    "Air": ["Spiraling Typhoon", "Whirling Tornado", "Storm Draft", "Cyclone Boomerang"],
+    "Earth": ["Shattering Strike", "Terra Ring", "Obsidian Cascade", "Rock N' Roll"],
+    "Lightning":["Shock Nova", "Shock Assault", "Volt Pinball", "Ball Lightning"],
+    "Water": ["Snowflake Chakrams", "Glacial Cross", "Frost Fan", "Rebounding Icicles"],
+    "Chaos": ["Mortal Coil", "Arcane Intervention", "Null Parade", "Distortion Beam", "Phantom Brigade", "Absolute Finale"]
+}
+
+bannedNames = [
+    "Pride",
+    "Friendship Bracelet",
+    "Museum Ticket",
+    "Insignia of Legend"
+]
+
+
+function pickArcana(arcanaDict, disallowedName) {
+    var elements = ["Fire", "Air", "Earth", "Lightning", "Water", "Chaos"];
+    var count = 0;
+    for(var i = 0, len = elements.length; i < len; ++i) {
+        var element = elements[i];
+        count += arcanaDict[element].length;
+    }
+    arcana = "";
+    arcanaType = "";
+    arcanaPosition = 0;
+    do {
+        arcanaIdx = Math.floor(Math.random() * count);        
+        for(var i = 0, len = elements.length; i < len; ++i) {
+            var element = elements[i];
+            if(arcanaIdx < arcanaDict[element].length) {
+                arcana = arcanaDict[element][arcanaIdx];
+                arcanaType = element;
+                arcanaPosition = arcanaIdx;
+                break;
+            } else {
+                arcanaIdx -= arcanaDict[element].length;
+            }
+        }
+    } while (bannedNames.indexOf(arcana) > -1 && arcana != disallowedName)
+    
+    return {"arcana": arcana, "arcanaType": arcanaType, "arcanaPosition": arcanaPosition};
+}
+
+function setArcanaDiv(id, arcanaInfo) {
+    var div = document.getElementById(id);
+    div.innerHTML = arcanaInfo["arcana"] + " (" + arcanaInfo["arcanaType"] + " #" + (arcanaInfo["arcanaPosition"] + 1) + ")";
+    div.className = "build-thing " + arcanaInfo["arcanaType"];
+}
+
+function generate() {
+    var outfit = "";
+    do {
+        outfit = outfits[Math.floor(Math.random() * outfits.length)];
+    } while(bannedNames.indexOf(outfit) > -1);
+    
+    
+    var relicCount = offenseRelics.length + defenseRelics.length + miscRelics.length;
+    var relic = "";
+    var relicType = "";
+    var relicPosition = 0;
+    do {
+        var relicIdx = Math.floor(Math.random() * relicCount);
+        if(relicIdx < offenseRelics.length) {
+            relic = offenseRelics[relicIdx]; 
+            relicType = "Offense";
+            relicPosition = relicIdx;
+        } else {
+            relicIdx -= offenseRelics.length;
+            if(relicIdx < defenseRelics.length) {
+                relic = defenseRelics[relicIdx];  
+                relicType = "Defense";
+                relicPosition = relicIdx;              
+            } else {
+                relicIdx -= defenseRelics.length;
+                relic = miscRelics[relicIdx];
+                relicType = "Misc";
+                relicPosition = relicIdx;
+            }
+        }
+    } while(bannedNames.indexOf(relic) > -1);
+    
+    basic = pickArcana(basicArcana, "");
+    dash = pickArcana(dashArcana, "");
+    signature = pickArcana(signatureArcana, "");
+    standard = pickArcana(standardArcana, signature["arcana"]);    
+    
+    var outfitDiv = document.getElementById("outfit");
+    outfitDiv.innerHTML = outfit;
+    
+    var relicDiv = document.getElementById("relic");
+    relicDiv.innerHTML = relic + " (" + relicType + " #" + (relicPosition + 1) + ")";
+    
+    setArcanaDiv("basic", basic);
+    setArcanaDiv("dash", dash);
+    setArcanaDiv("standard", standard);
+    setArcanaDiv("signature", signature);
+    
+}
+
+window.onload = generate;
